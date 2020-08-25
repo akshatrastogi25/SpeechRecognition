cd549439e3e88a970ae696c6be7f6ee0cdb58e97
akshatrastogi25 <42384464+akshatrastogi25@users.noreply.github.com>
2020-08-25 11:35:34 +0530

    Add files via upload

---
+++ Speechrec.py
@@ -0,0 +1,12 @@
+import speech_recognition as speech 
+
+rec=speech.Recognizer()
+
+with speech.Microphone() as source:
+    print("Please say something: ")
+    audio=rec.listen(source)
+    try:
+        result=rec.recognize_google(audio)
+        print("You just said: {}".format(result))
+    except:
+        print("Sorry! Unable to understand what you just said.")
